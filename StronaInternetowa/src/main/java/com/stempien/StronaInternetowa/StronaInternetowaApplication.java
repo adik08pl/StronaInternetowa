dd8380fbdb78eae975650c6113b3bf9f07896458
Adik08 <adik08.eamil@gmail.com>
2024-01-12 12:23:30 +0100

    Działa c:

---
+++ StronaInternetowa/src/main/java/com/stempien/StronaInternetowa/StronaInternetowaApplication.java
@@ -0,0 +1,13 @@
+package com.stempien.StronaInternetowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaInternetowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StronaInternetowaApplication.class, args);
+	}
+
+}
